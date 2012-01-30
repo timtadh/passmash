317f8549684422730bda6baeb6aa07ff57880137
Tim Henderson <tim.tadh@gmail.com>
2012-01-30 14:39:57 -0500

    made a setup file
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import passmash
+
+setup(name='swork',
+      version=str(passmash.RELEASE),
+      description=( 'A site specific password munger.'),
+      author='Tim Henderson',
+      author_email='tim.tadh@gmail.com',
+      url='https://www.github.com/timtadh/passmash/tree/%s' % str(passmash.RELEASE),
+      license='GPLv2',
+      scripts=['pm'],
+      py_modules=['passmash'],
+      platforms=['unix', 'darwin', 'windows'],
+)
+
