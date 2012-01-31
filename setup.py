93178829457551abcb910e59b23debe4892c180a
Tim Henderson <tim.tadh@gmail.com>
2012-01-31 11:50:37 -0500

    setup.py now uses readme.md cheeseshop didnt like symlinks
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(name='passmash',
       scripts=['pm'],
       py_modules=['passmash'],
       platforms=['unix', 'darwin', 'windows'],
-      data_files=[('.', ['GPL', 'LICENSE', 'README'])],
+      data_files=[('.', ['GPL', 'LICENSE', 'README.md'])],
 )
 
